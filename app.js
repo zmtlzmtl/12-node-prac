716f48dc4e34aab52028a090231cf853208c69f6
DONGYUN SHIN <windsdy1@gmail.com>
2023-02-07 09:45:27 +0900

    get, post, put, delete

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Helo World!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+    res.send('Got a put request at /user');
+});
+
+app.delete('/user', function (req, res) {
+    res.send("Got a DELETE request at /user");
+});
+
