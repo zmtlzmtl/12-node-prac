61cce43c4f2515cdbb1374212b72ed2aa200041f
DONGYUN SHIN <windsdy1@gmail.com>
2023-02-07 19:26:11 +0900

    .env를 사용하여 mongoDB password를 가리기

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,19 @@
- 
+require('dotenv').config();
+const { MONGODB_URI } = process.env;
+
+const mongoose = require("mongoose");
+
+const connect = () => {
+  mongoose
+    .connect(MONGODB_URI)
+    .catch(err => console.log(err));
+}
+
+mongoose.connection.on("error", err => {
+  console.error("몽고디비 연결 에러", err);
+});
+
+module.exports = connect;
+
+
+// mongodb+srv://test:<password>@cluster0.uazu7ta.mongodb.net/?retryWrites=true&w=majority
